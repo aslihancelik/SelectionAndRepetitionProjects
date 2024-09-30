d1e460e8fdbb04a09bdfbfb15b9bb88837c84270
aslihancelik <aslihancelik27@gmail.com>
2024-09-30 17:34:44 -0400

    in progress

--- SelectionAndRepetitionProjects.cpp
+++ SelectionAndRepetitionProjects.cpp
@@ -3,8 +3,37 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int number1, number2;
+    char operatorSelected;
+
+    cout << "Please enter the calculation you would like to make as:"  << endl;
+    cout << "number operator number " << endl << "(e.g. 5 + 5 or 6 / 1)" << endl;
+    cout << "using one of the following operators : " << endl;
+    cout << "+ for summation" << endl << "- for subtraction" << endl << "/ for division " << endl << "* for multiplication" << endl;
+    cout << "Then press enter.";
+
+    cin >> number1 >> operatorSelected >> number2;
+    //check if entered correct
+
+    do {
+        switch (operatorSelected) {
+        case '+':
+            break;
+        case '-':
+            break;
+        case '*':
+            break;
+        case '/':
+            break;
+        default:
+            cout << " Unknown operator.";
+            break;
+        }
+    } while();
+
+    return 0;
 }
